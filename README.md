9d1a7eace8917bfe668631a0cdcfb65b6d6ca39e
Calum Bruton <cbruton@konradgroup.com>
2019-01-22 16:42:49 -0500

    Setup color picker

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 * cd to frontend directory
 * npm install 
 * npm start
+
+
+
+TODO:
+* get color from color picker and pass to canvas
+* Check why canvas hand isn't exactly aligned with drawing
+* make a quick login page
+* Implement Routes
--- src/javascript/components/chatBox.jsx
+++ src/javascript/components/chatBox.jsx
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
 import DrawableCanvas from './drawableCanvas.jsx'
+import CircularColor from 'react-circular-color';
 
 class ChatBox extends Component {
+    constructor(props) {
+        super(props);
+        this.child = React.createRef();
+      }
+
+      state={
+          currentColor: "black",
+      }
+
+      handleClearOnClick() {
+          this.child.current.resetCanvas();
+      }
 
     render() {
         return (
             <div className = "chatbox-container">
                 <div className = "canvas-container">
-                    <DrawableCanvas />
+                    <DrawableCanvas ref={this.child}/>
                 </div>
                 <div className = "buttons-container">
-                    <button className = "clear-canvas-button">Clear</button>
+                    <button className = "clear-canvas-button" onClick={() => this.handleClearOnClick()}>Clear</button>
                     <button className = "send-button">Send</button>
+                    <div className="center-button"><CircularColor size={50} onChange={this.handleColorChange}/></div>
                 </div>
             </div>
         )
