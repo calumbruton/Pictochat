affb8df4c8cc6ed20bf04d9de5d56cafc96930e5
Calum Bruton <calumbruton@gmail.com>
2019-02-06 23:40:42 -0500

    Small fixes

--- Backend/queries.js
+++ Backend/queries.js
@@ -16,8 +16,10 @@ const getUsers = (request, response) => {
     })
   }
 
-  const getUserById = (request, response) => {
-    const email = parseInt(request.params.email)
+const getUserById = (request, response) => {
+    console.log(request.params)
+    const email = request.params.email
+    console.log(email)
   
     pool.query('SELECT * FROM users WHERE email = $1', [email], (error, results) => {
       if (error) {
@@ -25,22 +27,22 @@ const getUsers = (request, response) => {
       }
       response.status(200).json(results.rows)
     })
-  }
+}
 
-  const createUser = (request, response) => {
-    const { name, email } = request.body
+const createUser = (request, response) => {
+    const { email, password } = request.body
   
-    pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
+    pool.query('INSERT INTO users (email, password) VALUES ($1, $2)', [email, password], (error, results) => {
       if (error) {
         throw error
       }
-      response.status(201).send(`User added with ID: ${result.insertId}`)
+      response.status(201).send(`User added with email: ${result.insertEmail}`)
     })
-  }
+}
 
-  const updateUser = (request, response) => {
+const updateUser = (request, response) => {
     const email = parseInt(request.params.email)
-    const { email, password } = request.body
+    const { password } = request.body
   
     pool.query(
       'UPDATE users SET email = $1, password = $2 WHERE email = $3',
@@ -50,24 +52,25 @@ const getUsers = (request, response) => {
           throw error
         }
         response.status(200).send(`User modified with ID: ${email}`)
-      }
+        }
     )
-  }
+}
 
-  const deleteUser = (request, response) => {
-    const email = parseInt(request.params.email)
+const deleteUser = (request, response) => {
+    const email = request.params.email
   
     pool.query('DELETE FROM users WHERE email = $1', [email], (error, results) => {
       if (error) {
         throw error
       }
-      response.status(200).send(`User deleted with ID: ${email}`)
+      response.status(200).send(`User deleted with email: ${email}`)
     })
-  }
+}
 
-  module.exports = {
+module.exports = {
     getUsers,
     getUserById,
     createUser,
+    updateUser,
     deleteUser,
-  }
+}
